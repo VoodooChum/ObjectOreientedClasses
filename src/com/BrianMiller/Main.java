c107c9b156aa05e5b713d9ab8fc85ae2363e42a9
VoodooChum <brianmillernola@gmail.com@mail.com>
2021-09-14 16:01:02 -0500

    VipPerson class added

--- src/com/BrianMiller/Main.java
+++ src/com/BrianMiller/Main.java
@@ -10,5 +10,12 @@ public class Main {
 	    holden.setModel("Junker");
         System.out.println(porsche.getModel());
         System.out.println(holden.getModel());
+
+        VipPerson person1 = new VipPerson();
+        VipPerson person2 = new VipPerson("Bob", 25000.00);
+        VipPerson person3 = new VipPerson("Brian", 50000, "brian@email.com");
+        System.out.println(person1.getName() + " " + person1.getCreditLimit() + " " + person1.getEmailAddress());
+        System.out.println(person2.getName() + " " + person2.getCreditLimit() + " " + person2.getEmailAddress());
+        System.out.println(person3.getName() + " " + person3.getCreditLimit() + " " + person3.getEmailAddress());
     }
 }
