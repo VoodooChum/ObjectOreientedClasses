a887b3b873bcd67f33a1cb1302ba2ffcf3da92ae
brimill <brian.miller.ext@sodexo.com>
2021-09-14 10:06:17 -0500

    Set fields of car class

---
+++ src/com/BrianMiller/Car.java
@@ -0,0 +1,11 @@
+package com.BrianMiller;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String  color;
+
+}
